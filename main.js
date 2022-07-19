476724a258ca88b508aac44a3076c5191396a500
raguila6 <raguila6@stu.southtexascollege.edu>
2022-07-18 22:05:57 -0500

    menuupdated

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 import './node_modules/the-new-css-reset/css/reset.css'
 import './style.css'
+
+const menuToggle = document.querySelector('.menu-toggle')
+const menu = document.querySelector('.menu')
+
+menuToggle.addEventListener('click', () => menu.classList.toggle('hidden'))
