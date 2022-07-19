0fd99c625c200bb6245d3fb944ab3e2a12380c2e
raguila6 <raguila6@stu.southtexascollege.edu>
2022-07-19 18:21:06 -0500

    menucompleteion

--- main.js
+++ main.js
@@ -4,4 +4,16 @@ import './style.css'
 const menuToggle = document.querySelector('.menu-toggle')
 const menu = document.querySelector('.menu')
 
+const initMenu = () => {
+    if (window.innerWidth < 768)  {
+      menu.classList.add('hidden')
+    } else
+    {
+      menu.classList.remove('hidden')
+    }
+}
+
 menuToggle.addEventListener('click', () => menu.classList.toggle('hidden'))
+window.addEventListener('resize', initMenu)
+
+initMenu()
